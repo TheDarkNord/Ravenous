774f068de593c964245c142dedd08ad4d4bb3766
TheDarkNord <30898148+TheDarkNord@users.noreply.github.com>
2018-01-31 18:20:17 +0200

    Create Git

---
+++ ravenous/src/util/Yelp.js
@@ -0,0 +1,30 @@
+const apiKey = 'LdMdlEgirsTDxioBua3H87QPUoIDza5TOTi7Q_EPNv9g6_xWZuv5vwepPq3PkE6vV47-uaPBchSXeqH4QIm9YglJ1XErB3GhooH1SYPe3LFYLSAn4nqYnJneu31xWnYx';
+
+const Yelp = {
+  search(term, location, sortBy) {
+    return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+      headers: {
+        Authorization: `Bearer ${apiKey}`
+      }
+    }).then(response => {
+      return response.json();
+    }).then(jsonResponse => {
+      if (jsonResponse.businesses) {
+        return jsonResponse.businesses.map(business => ({
+          id: business.id,
+          imageSrc: business.image_url,
+          name: business.name,
+          address: business.location.address1,
+          city: business.location.city,
+          state: business.location.state,
+          zipCode: business.location.zip_code,
+          category: business.categories[0].title,  //issue with categories
+          rating: business.rating,
+          reviewCount: business.review_count
+        }));
+      }
+    });
+  }
+};
+
+export default Yelp;
